2eb0322fb372f7b7cab2760efe1f984205d4eaac
Vjfrib <107771171+Vjfrib@users.noreply.github.com>
2023-08-03 12:57:52 +0000

    colocando menu

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-
+const rls = require("readline-sync");
 const { nomes } = require("./func.js")
 
 //import { nomes } from './func.js';
+var index = parseInt(rls.question("1. Nomes\n2. Fibonacci"));
+console.log(index)
 
 var mine = 'Vinicius Jose Fernandes' //Debug armazenado no programa
 console.log("Iniciando a função de nomes com variáveis locais");
