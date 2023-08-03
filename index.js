e21d53d79691dd7dc0543584c60ee421c379ce55
Vjfrib <107771171+Vjfrib@users.noreply.github.com>
2023-08-03 13:19:42 +0000

    menu funcional

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const rls = require("readline-sync");
+//Como aqui tbm pede pro menu, ent puxa dnv
 const { nomes } = require("./func.js")
 
-//import { nomes } from './func.js';
-var index = parseInt(rls.question("1. Nomes\n2. Fibonacci"));
-console.log(index)
+//functions
+function menu () {
+  var index = parseInt(rls.question("1. Nomes\n2. Fibonacci\n3. Sair\n"));
+  console.log(index)
 
-var mine = 'Vinicius Jose Fernandes' //Debug armazenado no programa
-console.log("Iniciando a função de nomes com variáveis locais");
-nomes(mine);
+  switch(index){
+      case 1:
+      var mine = 'Vinicius Jose Fernandes' //Debug armazenado no programa
+      nomes(mine); 
+      break;
+    case 2:
+      console.log("Iniciando a função de nomes com variáveis locais");
+      break;
+    case 3:
+      process.exit();
+    default:
+      menu();
+  }
+    
+}
+
+//L.A.D.S
+menu();
+//import { nomes } from './func.js';
